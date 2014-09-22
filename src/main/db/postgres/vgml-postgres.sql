f8e8f969bef6429b0f2b4aaacf8cf60d4fdb82ce
Ben Caradoc-Davies <Ben.Caradoc-Davies@csiro.au>
2014-09-22 15:13:30 +0800

    Completed postgres support

--- src/main/db/postgres/vgml-postgres.sql
+++ src/main/db/postgres/vgml-postgres.sql
@@ -3,6 +3,9 @@
 drop table jobs;
 drop table series;
 drop table jobs_audit_log;
+drop table downloads;
+drop table parameters;
+drop table signatures;
 
 create table "series" (
   "id" serial primary key,
@@ -46,3 +49,47 @@ create table "jobs_audit_log" (
     "message" text
 );
 create index "idx_jobs_audit_log_jobid" on "jobs_audit_log" ("jobid");
+
+create table "downloads" (
+    "id" serial primary key,
+    "jobid" integer not null,
+    "url" text not null,
+    "localpath" text not null,
+    "name" text,
+    "description" text,
+    "northboundlatitude" float,
+    "southboundlatitude" float,
+    "eastboundlongitude" float,
+    "westboundlongitude" float
+);
+create index "idx_downloads_jobid" on "downloads" ("jobid");
+
+create table "parameters" (
+    "id" serial primary key,
+    "jobid" integer not null,
+    "name" text not null,
+    "value" text,
+    "type" text not null
+);
+create index "idx_parameters_jobid" on "parameters" ("jobid");
+
+create table "signatures" (
+    "id"  serial primary key,
+    "user" text not null,
+    "individualname" text,
+    "organisationname" text,
+    "positionname" text,
+    "telephone" text,
+    "facsimile" text,
+    "deliverypoint" text,
+    "city" text,
+    "administrativearea" text,
+    "postalcode" text,
+    "country" text,
+    "onlinecontactname" text,
+    "onlinecontactdescription" text,
+    "onlinecontacturl" text,
+    "keywords" text,
+    "constraints" text
+);
+create index "idx_signatures_user" on "signatures" ("user");
